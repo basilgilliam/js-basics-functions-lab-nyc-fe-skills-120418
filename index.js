ba509e4addcd6935ba3ae862622f7fccbee3f386
Basil Gilliam <basilgilliam@gmail.com>
2019-01-18 01:22:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,9 @@
 const startingPoint = 42
 function distanceFromHqInBlocks(blocks){
  return blocks - startingPoint
+ if (blocks < startingPoint){
+  return startingPoint - blocks
+ } else {
+   return blocks - startingPoint
+ }
 }
