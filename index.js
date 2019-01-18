39288ff852bcd0b6229febac0091374dd9cb6b12
Basil Gilliam <basilgilliam@gmail.com>
2019-01-18 01:25:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solution in this file!
 const startingPoint = 42
 function distanceFromHqInBlocks(blocks){
- return blocks - startingPoint
  if (blocks < startingPoint){
   return startingPoint - blocks
  } else {
